94a1f55775bcd53234c419ed40d91d7d67648552
trista_lea <tristan_lea@outlook.com>
2023-03-20 14:50:45 -0700

    changed .env, and dotenv config

--- config/connection.js
+++ config/connection.js
@@ -1,16 +1,15 @@
 require('dotenv').config();
-require('dotenv').config({ path: 'path/to/.env' });
 
 const Sequelize = require('sequelize');
 
 const sequelize = process.env.JAWSDB_URL
   ? new Sequelize(process.env.JAWSDB_URL)
-  : new Sequelize('ecommerce_db', 'root', 'Aperture1', {
+  : new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {
       host: 'localhost',
       dialect: 'mysql',
       dialectOptions: {
         decimalNumbers: true,
       },
     });
-  
+
 module.exports = sequelize;
